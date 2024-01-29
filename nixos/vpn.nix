612e73d7ba6ab8f4cad1531603c39e3e067182ce
linozen <linus@sehn.dev>
2024-01-29 09:28:04 +0100

    feat(vpn): refactor for mullvad exit node usage

--- nixos/vpn.nix
+++ nixos/vpn.nix
@@ -4,30 +4,17 @@
     enable = true;
     package = pkgs.unstable.tailscale;
   };
-  networking.nameservers = ["100.100.100.100" "8.8.8.8" "149.112.112.112"];
+  networking.nameservers = ["100.100.100.100" "194.242.2.2" "2a07:e340::2"];
   networking.search = ["wolf-map.ts.net"];
 
-  # Enable Mullvad
-  services.mullvad-vpn = {
-    enable = true;
-    package = pkgs.unstable.mullvad-vpn;
-  };
+  environment.systemPackages = with pkgs; [
+    nftables
+    iptables
+  ];
 
-  # https://mullvad.net/en/help/split-tunneling-with-linux-advanced/
-  environment.etc."nft/mullvadExcludeTraffic.rules".text = ''
-    define EXCLUDED_IPS = {
-        100.94.222.31,
-        100.111.94.93,
-        100.115.207.15
-    }
+  # Based on https://github.com/tailscale/tailscale/issues/4432#issuecomment-1112819111
+  networking.firewall.checkReversePath = "loose";
 
-    table inet excludeTraffic {
-      chain excludeOutgoing {
-        type route hook output priority -100; policy accept;
-        ip daddr $EXCLUDED_IPS ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
-      }
-    }
-  '';
-
-  networking.firewall.extraCommands = "${pkgs.nftables}/bin/nft -f /etc/nft/mullvadExcludeTraffic.rules";
+  # No IPv6 yet when using Mullvad Exit node
+  # https://github.com/tailscale/tailscale/issues/9511
 }
