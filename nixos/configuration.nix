582a36a2d9defa06c73d07ae69f836dd62408b8c
linozen <linus@sehn.dev>
2024-01-29 09:34:11 +0100

    fix(calendars): enable evolution for cal setup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -121,6 +121,10 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  programs.evolution = {
+    enable = true;
+    # plugins = [ ];
+  };
 
   # Enable Yubikey
   services.udev.packages = [pkgs.yubikey-personalization pkgs.android-udev-rules];
