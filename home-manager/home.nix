f3b827db00969f997364cf53a205b3f0025cbec4
linozen <linus@sehn.dev>
2024-05-05 09:06:39 +0200

    feat: start configuring hyprland

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -181,6 +181,11 @@
     };
   };
 
+  home.keyboard = {
+    layout = "gb";
+    options = ["caps:escape"];
+  };
+
   # Enable programs with home-manager integration
   programs.home-manager.enable = true;
   programs.git.enable = true;
@@ -189,6 +194,85 @@
     enable = true;
     # enableFishIntegration = true; # see note on other shells below
     nix-direnv.enable = true;
+  };
+
+  # Configure hyprland
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      input = {
+        kb_layout = "gb";
+        kb_options = "caps:escape";
+      };
+      xwayland = {
+        force_zero_scaling = true;
+      };
+      bind =
+        [
+          "$mod, F, exec, firefox"
+          "$mod, T, exec, kitty"
+          ", Print, exec, grimblast copy area"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
+    };
+    extraConfig = ''
+      monitor=eDP-1,preferred,auto,2
+      monitor=DP-1,preferred,auto-right,1.5
+      monitor=,preferred,auto,1
+    '';
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+
+    font = {
+      name = "Inter Medium";
+      size = 10;
+    };
+  };
+
+  # Confgiure kitty terminal
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "Iosevka Nerd Font";
+      size = 11;
+    };
+    theme = "Tokyo Night";
+    shellIntegration.enableFishIntegration = true;
   };
   programs.fish = {
     enable = true;
